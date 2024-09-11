c56b6ac091d4181f0126f7029a615fdac28d4cfc
brian martinez <bmartinez@brians-MacBook-Air.local>
2024-09-11 15:59:42 -0700

    more hp stuff

--- src/classes/entities/hitpoints/hitpoints.h
+++ src/classes/entities/hitpoints/hitpoints.h
@@ -9,20 +9,48 @@ date: 9/11/24
 class hp {
 public:
     //returns true if set successfully
-    bool setMaxHP (hptype new_max_hp) {
-    if (new_max_hp < 1)
-	return false;
-    if (CurrentHP < new_max_hp)
-	CurrentHP = new_max_hp;
-    return true;
+    bool setMaxHP (hptype new_max_hp)
+    {
+	if (new_max_hp < 1)
+	    return false;
+	if (CurrentHP < new_max_hp)
+	    CurrentHP = new_max_hp;
+        return true;
+    }
+    hptype getMaxHP()
+    {
+	return MaxHP;
     }
 
-    bool takeDmg (hptype dmg_taken) {
+    hptype getCurrentHP()
+    {
+	return CurrentHP;
+    }
+
+    void takeDmg (hptype dmg_taken)
+    {
+	if (dmg_taken > CurrentHP) {
+	    CurrentHP = 0; 
+	    return;
+	}
+	
+	CurrentHP -= dmg_taken;
+    }
+
+    void heal (hptype healAmt)
+    {
+	if ((healAmt + CurrentHP) > MaxHP){
+	    CurrentHp = MaxHP
+	    return;
+	}
+
+	CurrentHP += healAmt;
+    }
     
 
 
 private:
     hptype CurrentHP;
-    hptype MaxHp;
+    hptype MaxHP;
 	
 };
