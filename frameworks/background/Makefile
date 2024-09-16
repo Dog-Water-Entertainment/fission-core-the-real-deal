3a531ac7120376e028bb591967a6ef709a137481
Michael Guillory <54484091+ToonCra123@users.noreply.github.com>
2024-09-16 09:17:40 -0700

    Delete frameworks directory

--- frameworks/background/Makefile
+++
@@ -1,9 +0,0 @@
-all: background
-
-background: background.cpp
-	g++ background.cpp -Wall -lX11 -lGL -lGLU -lm
-
-clean:
-	rm -f background a.out
-
-
--- frameworks/ggfonts/log.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LOG_H_
-#define _LOG_H_
-
-extern void logOpen();
-extern void logClose();
-extern void Log(const char *fmt, ...);
-
-#endif
