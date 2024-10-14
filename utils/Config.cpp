6531803940e94a9c08fd98138e81fc226b459239
Mykull guillory <michaelguillory.mg@gmail.com>
2024-10-14 15:59:58 -0700

    Weird implementtion of config file, very innefficient, but should work.

--- utils/Config.cpp
+++ utils/Config.cpp
@@ -3,6 +3,85 @@
 // Implementation of a 
 // config file
 
+
+/*
+*
+* Very Innefficent way to load a config file
+* If you have a better way to load a config file
+* please refactor this code using hashing or some other
+*
+*/
+#include "Config.h"
+#include <fstream>
+
 ConfigLoader::ConfigLoader(std::string filename) {
 	this->FILE = filename;
+}
+
+
+// Helper function to see if the line 
+// is a comment
+bool shouldContinue(std::string in) {
+	return (in[0] == ';');
+}
+
+int ConfigLoader::getInt(std::string key) {
+	std::ifstream _file(this->FILE);
+	std::string line;
+	while (std::getline(_file, line)) {
+		if (shouldContinue(line)) continue;
+		if (line.find(key) != std::string::npos) {
+			_file.close();
+			return std::stoi(line.substr(line.find("=") + 1));
+		}
+	}
+	_file.close();
+	return -1;
+}
+
+std::string ConfigLoader::getString(std::string key) {
+	std::ifstream _file(this->FILE);
+	std::string line;
+	while (std::getline(_file, line)) {
+		if (shouldContinue(line)) continue;
+		if (line.find(key) != std::string::npos) {
+			_file.close();
+			return line.substr(line.find("=") + 1);
+		}
+	}
+	_file.close();
+	return "";
+}
+
+bool ConfigLoader::getBool(std::string key) {
+	std::ifstream _file(this->FILE);
+	std::string line;
+	while (std::getline(_file, line)) {
+		if (shouldContinue(line)) continue;
+		if (line.find(key) != std::string::npos) {
+			_file.close();
+			return line.substr(line.find("=") + 1) == "true";
+		}
+	}
+	_file.close();
+	return false;
+}
+
+float ConfigLoader::getFloat(std::string key) {
+	std::ifstream _file(this->FILE);
+	std::string line;
+	while (std::getline(_file, line)) {
+		if (shouldContinue(line)) continue;
+		if (line.find(key) != std::string::npos) {
+			_file.close();
+			return std::stof(line.substr(line.find("=") + 1));
+		}
+	}
+	_file.close();
+	return -1.0f;
+}
+
+ConfigLoader::~ConfigLoader() {
+	// Destructor
+	// Nothing to do here
 }
--- utils/Config.h
+++ utils/Config.h
@@ -1,12 +1,19 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
-
+#include <string>
 
 class ConfigLoader {
 std::string FILE;
 public:
     ConfigLoader(std::string);
     ~ConfigLoader();
+
+    int getInt(std::string key);
+    float getFloat(std::string key);
+    std::string getString(std::string key);
+    bool getBool(std::string key);
+
+
 };
 
 #endif
