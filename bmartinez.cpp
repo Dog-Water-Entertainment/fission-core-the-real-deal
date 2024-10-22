e6667ca568bb28932344b5ec43b590692f917e37
Brian Martinez <bmartinez@odin.cs.csubak.edu>
2024-10-22 15:31:47 -0700

    backbone of the health and dmg stuff, items, and death quips added - still wip

--- bmartinez.cpp
+++ bmartinez.cpp
@@ -1,12 +1,15 @@
-//brian martinez source file.
-//
-//
-//
-//
-//
+//author: brian martinez
+//original start date: ?
+//current date: 10/22/24
 #include <GL/glx.h>
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <string>
+#include "bmartinez.h"
+typedef int Stat;
+// Forward Declarations
+class Player;
+class Enemy;
 void darkMode(bool click, int xres, int yres)
 {
      if (click) {
@@ -21,14 +24,90 @@ void darkMode(bool click, int xres, int yres)
             glVertex2i(xres, yres);
          glEnd();
          glEnable(GL_TEXTURE_2D);
-         //glDisable(GL_BLEND);
-         //glBlendFunc(GL_ONE, GL_ZERO);
          glDisable(GL_BLEND);
      } else {
-
-         //glBlendFunc(GL_ONE, GL_ZERO);
          glDisable(GL_BLEND);
      } 
 };
+//PLAYER STUFF/////////////////////////////////////////////////////////////////
+Player::Player(Stat hp, Stat dmg) : MaxHP(hp), HP(hp), dmgDeal(dmg) {}
+Player::Player() : MaxHP(50), HP(50), dmgDeal(2) {}
 
+void Player::Attack(Enemy &a, Stat dmg)
+{
+    a.HP -= dmg;
+    if (a.HP < 0)
+        a.HP = 0;
+}
+void Player::Heal () 
+{
+}
+///////////////////////////////////////////////////////////////////////////////
+///
+//ENEMY STUFF//////////////////////////////////////////////////////////////////
+Enemy::Enemy(Stat hp, Stat) : MaxHP(hp), HP(50), dmgDeal(2) {}
+Enemy::Enemy() : MaxHP(50), HP(50), dmgDeal(2) {}
+void Enemy::Attack(Player &a, Stat dmg)
+{
+    a.HP -= dmg;
+    if (a.HP < 0)
+        a.HP = 0;
+}
+void Enemy::Heal()
+{
+        
+}
+///////////////////////////////////////////////////////////////////////////////
+///
+// ITEM STUFF//////////////////////////////////////////////////////////////////
+Item::Item(int ID) : itemID(ID) 
+{
+    std::cout << "placeholder" << ID  << std::endl;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///
+// DEATH STUFF////////////////////////////////////////////////////////////////
+void youDied()
+{
+    std::vector<std::string> quips{
+    "You are trying, right?",  
+    "Consider Fortnite!", 
+    "Consider Roblox!", 
+    "Us: 99999, you: 0", 
+    "git gud...",
+    "You suck.",
+    "Consider the Talk-Tuah Podcast...", 
+    "on skibidi ????", 
+    "We've been trying to reach you concerning your vehicle's extended warranty.",
+    "*teleports behind you* nothing personal, kid...", 
+    "What the flip did you just flipping say about me, you little rascal? "
+    "I'll have you know I graduated top of my class in the Navy Seals, and "
+    "I've been involved in numerous secret raids on Al-Quaeda, and I have over"
+    " 300 confirmed kills. I am trained in gorilla warfare and I'm the top "
+    "sniper in the entire US armed forces. You are nothing to me but just "
+    "another target. I will wipe you the flip out with precision the likes of "
+    "which has never been seen before on this Earth, mark my flipping words. "
+    "You think you can get away with saying that crap to me over the Internet? "
+    "Think again, little guy. As we speak I am contacting my secret network of "
+    "spies across the USA and your IP is being traced right now so you better "
+    "prepare for the storm, maggot. The storm that wipes out the pathetic "
+    "little thing you call your life. You're flippin' dead, kid. I can be "
+    "anywhere, anytime, and I can kill you in over seven hundred ways, and "
+    "that's just with my bare hands. Not only am I extensively trained in "
+    "unarmed combat, but I have access to the entire arsenal of the United "
+    "States Marine Corps and I will use it to its full extent to wipe your "
+    "miserable cheeks off the face of the continent, you little poop. If only "
+    "you could have known what unholy retribution your little \"clever\" "
+    "comment was about to bring down upon you, maybe you would have held your "
+    "flipping tongue. But you couldn't, you didn't, and now you're paying the "
+    "price, you goshdarn nincompoop. I will poo fury all over you and you will "
+    "drown in it.You're frinkin dead, kiddo."
+    };
+    //int number = getrandomnumber(0, sizeofvector) 
+    //std::cout << quips[number] << endl;
+    // its supposed to write to the screen but ill figure that out once 
+    // the scene loader can be used or something idk, pick a random number
+    // spit out the line at that index
+}
 
--- bmartinez.h
+++ bmartinez.h
@@ -2,10 +2,52 @@
 //headers
 #ifndef BMARTINEZ_H
 #define BMARTINEZ_H
+typedef int Stat;
+//forward declarations
+class Enemy;
 
+class Player
+{
+    private:
+        Stat MaxHP;
 
+    public:
+        Stat HP;
+        Stat dmgDeal;  
+        Player(Stat hp, Stat dmg);
+        Player();
+        void Attack(Enemy &a, Stat dmg);
+        void Heal (); 
+};
 
+class Enemy
+{
+    private:
+        Stat MaxHP;
 
-#endif
+    public:
+        Stat HP;
+        Stat dmgDeal;
+        Enemy(Stat hp, Stat dmg);
+        Enemy();
+        void Attack(Player &a, Stat dmg);
+        void Heal();
+};
+
+class Item
+{
+    public:
+    int itemID;       // 1 = heals, 2 = super heals, 3 = buff (+10 max health 
+                      // temporarily) 4 = minor debuff (-10 max health) 
+                      // idk just ideas.
+    int duration;     //duration of the effect (if buff/debuff)
+    Item(int ID);
+
+//ill figure a system out eventually
+};
+
 
 void darkMode(bool click, int xres, int yres);
+void youDied();
+
+#endif
